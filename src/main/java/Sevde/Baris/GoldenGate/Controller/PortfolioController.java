212d3b60ccb2cd82bbf05eddb2ddbf2dc4aada64
Sevde Secer <sevdeseceerr@gmail.com>
2023-12-24 10:47:25 +0300

    Add css and html.(Portfolio and Selected Portfolio)

--- src/main/java/Sevde/Baris/GoldenGate/Controller/PortfolioController.java
+++ src/main/java/Sevde/Baris/GoldenGate/Controller/PortfolioController.java
@@ -2,19 +2,16 @@ package Sevde.Baris.GoldenGate.Controller;
 
 import Sevde.Baris.GoldenGate.DTO.UserStock.UserStockGetAllResponseDTO;
 import Sevde.Baris.GoldenGate.Model.Portfolio;
-import Sevde.Baris.GoldenGate.Model.UserStock;
 import Sevde.Baris.GoldenGate.Service.Portfolio.PortfolioService;
 import Sevde.Baris.GoldenGate.Service.UserStock.UserStockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Controller
