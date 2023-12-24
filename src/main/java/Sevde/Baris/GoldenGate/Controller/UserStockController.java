cb8aece18d4fd6170bd5e66a88c6c347541e464f
Barış Atasoy <barisatasoy2917@gmail.com>
2023-12-24 19:31:18 +0300

    merge with Sevdecimmm Seceerrr <3

--- src/main/java/Sevde/Baris/GoldenGate/Controller/UserStockController.java
+++ src/main/java/Sevde/Baris/GoldenGate/Controller/UserStockController.java
@@ -24,6 +24,7 @@ import java.util.UUID;
 @Controller()
 @RequestMapping("/StockDetail")
 public class UserStockController {
+
     @Autowired
     private IUserStockDetailService detailService;
     @Autowired
