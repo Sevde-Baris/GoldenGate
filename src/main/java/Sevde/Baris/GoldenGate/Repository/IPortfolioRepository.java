0c92ddc914951695e96fe6aa769b1530cfeea2a8
Barış Atasoy <barisatasoy2917@gmail.com>
2024-01-02 00:21:42 +0300

    Portfolio name update problem has been solved.

--- src/main/java/Sevde/Baris/GoldenGate/Repository/IPortfolioRepository.java
+++ src/main/java/Sevde/Baris/GoldenGate/Repository/IPortfolioRepository.java
@@ -3,7 +3,6 @@ package Sevde.Baris.GoldenGate.Repository;
 import Sevde.Baris.GoldenGate.Model.Portfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.UUID;
 
 @Repository
